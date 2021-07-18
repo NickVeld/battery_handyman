a7856d2a6394ab5b8d7902429c7c9d40a208fd6a
NickVeld <NickVeld@users.noreply.github.com>
2021-07-18 11:37:12 +0300

    docs: make hidden files marked more specifically

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,7 @@ templates_path = [
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-    "*/_*"
+    "*/__*"
 ]
 
 # -- Options for HTML output -------------------------------------------------
